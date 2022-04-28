baee10f5e810ee1f7680f260d03323beb9ca899a
EC2 Default User <ec2-user@ip-172-31-4-175.ap-northeast-1.compute.internal>
2022-04-28 13:56:55 +0000

    func_clickpoint

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 var resultArea;
 var is_calc = false;
+var inputNumber = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 window.addEventListener("load",function() {
+    resultArea = null;
     resultArea = document.getElementById('resultArea');
 })
 
 function clickClear() {
-    resultArea.value = 0;
+    resultArea.value = "0";
     is_calc = false;
     
     document.getElementById("inputPoint").disabled=false;
-    document.getElementById("inputZero").disabled=false;
-    document.getElementById("inputDoubleZero").disabled=false;
-    
 }
 
 function clickNumber(val) {
@@ -21,49 +20,47 @@ function clickNumber(val) {
 
     if(resultArea.value =="0" && val == "0"){
         resultArea.value = "0";
+    }else if(resultArea.value =="0" && val == "00"){
+        resultArea.value = "0";
     }else if(resultArea.value == "0"){
         resultArea.value = val;
     }else{
         resultArea.value += val;
     }
-    
-    document.getElementById("inputZero").disabled=false;
-    document.getElementById("inputDoubleZero").disabled=false;
-}
-
-function clickPoint(val) {
-    is_calc = false;  
-    
-    resultArea.value += val;
-    document.getElementById("inputPoint").disabled=true;
-}
-
-function clickZero(val) {
-    is_calc = false; 
-    
-    resultArea.value += val;
-}
-
-function clickDoubleZero(val) {
-    is_calc = false;  
-    
-    resultArea.value += val;
 }
 
 function clickFBAO(val) {
+    if(resultArea.value == "0") resultArea.value = val;
+    
     if(is_clickFBAO_last()) {
         resultArea.value = resultArea.value.slice(0, -1) + val;
-    } else {
+    }else {
         resultArea.value += val;
     }
-    
-    document.getElementById("inputPoint").disabled=true;
-    document.getElementById("inputZero").disabled=true;
-    document.getElementById("inputDoubleZero").disabled=true;
+}
+
+function clickPoint(val) {
+    if(can_clickPoint()) {
+        if(["."].includes.resultArea.value) {
+            if(["+","-","*","/"].includes.resultArea.value) {
+                if(resultArea.indexOf(".") < resultArea.indexOf("+","-","*","/") && resultArea.split(".").length === resultArea.split("+","-","*","/").length + 1) {
+                    resultArea.value += val;
+                }else {
+                    resultArea.value = resultArea.value.slice(0);
+                }
+            }else {
+                resultArea.value = resultArea.value.slice(0);
+            }
+        }else {
+            resultArea.value += val;
+        }
+    }else {
+        resultArea.value = resultArea.value.slice(0);
+    }
 }
 
 function clickEqual() {
-    if(is_clickFBAO_last())  resultArea.value = resultArea.value.slice(0, -1);
+    if(is_clickFBAO_last())  resultArea.value = resultArea.value.slice(0);
 
     var temp = new Function("return "  + resultArea.value)();
     if(temp == Infinity || Number.isNaN(temp)){
@@ -77,4 +74,8 @@ function clickEqual() {
 
 function is_clickFBAO_last() {
     return ["+","-","*","/"].includes(resultArea.value.toString().slice(-1));
+}
+
+function can_clickPoint() {
+    return inputNumber.includes(resultArea.value.toString().slice(-1));
 }
